03213db227787383820c0ba20b07891c98281c5f
Xinyi Yan <xyan@salesforce.com>
2019-10-15 13:19:21 -0700

    updated util function

--- phoenix-core/src/main/java/org/apache/phoenix/mapreduce/PhoenixIntegrityCheckAndRepairTool.java
+++ phoenix-core/src/main/java/org/apache/phoenix/mapreduce/PhoenixIntegrityCheckAndRepairTool.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.phoenix.mapreduce;
 
 import org.apache.hadoop.conf.Configured;
@@ -9,6 +26,7 @@ import org.apache.phoenix.schema.PTableType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.*;
@@ -19,6 +37,7 @@ public class PhoenixIntegrityCheckAndRepairTool extends Configured implements To
             LoggerFactory.getLogger(PhoenixIntegrityCheckAndRepairTool.class);
 
     private HashSet<PhckUtil.Row> orphanRowSet = new HashSet<>();
+    private HashMap<String, PhckUtil.Table> allTables = new HashMap<>();
     /**
      SELECT
         TENANT_ID, TABLE_SCHEM,TABLE_NAME,TABLE_TYPE,
@@ -27,16 +46,6 @@ public class PhoenixIntegrityCheckAndRepairTool extends Configured implements To
      FROM SYSTEM.CATALOG
         WHERE TABLE_SCHEM NOT LIKE 'SYSTEM%';
      */
-    private static final String selectQuery = "SELECT " +
-            TENANT_ID + ", " +
-            TABLE_SCHEM + "," +
-            TABLE_NAME + "," +
-            COLUMN_NAME + "," +
-            COLUMN_COUNT + "," +
-            TABLE_TYPE + "," +
-            " FROM " + SYSTEM_CATALOG_NAME +
-            " WHERE "+ TABLE_TYPE + " = '" + PTableType.VIEW.getSerializedValue() +"' AND NOT " +
-            VIEW_TYPE + " = " + PTable.ViewType.MAPPED.getSerializedValue();
 
 
 
--- phoenix-core/src/main/java/org/apache/phoenix/mapreduce/util/PhckUtil.java
+++ phoenix-core/src/main/java/org/apache/phoenix/mapreduce/util/PhckUtil.java
@@ -1,17 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.phoenix.mapreduce.util;
 
 import org.apache.phoenix.schema.PIndexState;
 import org.apache.phoenix.schema.PTableType;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_SCHEMA_NAME;
+import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.*;
+import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_CATALOG_NAME;
 
 public class PhckUtil {
+    public static final String BASE_SELECT_QUERY = "SELECT " +
+            TENANT_ID + ", " + TABLE_SCHEM + "," + TABLE_NAME + "," + TABLE_TYPE + "," +
+            COLUMN_FAMILY + "," + COLUMN_NAME + "," + COLUMN_COUNT + "," + TABLE_TYPE + "," +
+            INDEX_STATE + "," + INDEX_TYPE + "," + VIEW_TYPE + "," + COLUMN_QUALIFIER_COUNTER +
+            " FROM " + SYSTEM_CATALOG_NAME;
+
     public class Table {
         String tenantId;
-        String schema;
+        String tableSchema;
         String tableName;
         PTableType tableType;
         PIndexState indexState;
@@ -20,8 +45,51 @@ public class PhckUtil {
         // Number of columns belongs to a table
         int columnCounter;
         Table parent;
-        Table root;
+        Table physicalTable;
         List<Table> children;
+
+        public Table(String tenantId, String tableSchema, String tableName, PTableType tableType,
+                     int headRowColumnCount) {
+            this.tenantId = tenantId;
+            this.tableSchema = tableSchema;
+            this.tableName = tableName;
+            this.tableType = tableType;
+            this.headRowColumnCount = headRowColumnCount;
+            this.columnCounter = 0;
+        }
+
+        public int getParentTableHeadRowColumnCount() {
+            if (parent == null) {
+                return -1;
+            }
+
+            return parent.getHeadRowColumnCount();
+        }
+
+        public int getHeadRowColumnCount() {
+            return this.headRowColumnCount;
+        }
+
+        public void incrementColumnCount() {
+            columnCounter++;
+        }
+
+        public void addParentTable(Table parent) {
+            this.parent = parent;
+        }
+
+
+        public void addPhysicalTable(Table physicalTable) {
+            this.physicalTable = physicalTable;
+        }
+
+        public void addChildTable(Table child) {
+            if (children == null) {
+                children = new ArrayList<>();
+            }
+
+            children.add(child);
+        }
 
         public boolean isIndexDisabled() {
             if (this.indexState == PIndexState.DISABLE ||
@@ -30,6 +98,27 @@ public class PhckUtil {
                 return true;
             }
             return false;
+        }
+
+        public boolean isSystemTable() {
+            return this.tableSchema != null && this.tableSchema.equals(SYSTEM_SCHEMA_NAME) &&
+                    this.tableName != null &&
+                    this.tableType != null && this.tableType == PTableType.SYSTEM;
+        }
+
+        public boolean isIndexTable() {
+            return this.tableName != null &&
+                    this. tableType != null && this.tableType == PTableType.INDEX;
+        }
+
+        public boolean isPhysicalTable() {
+            return this.tableName != null &&
+                    this.tableType != null && this.tableType == PTableType.TABLE;
+        }
+
+        public boolean isViewTable() {
+            return this.tableName != null &&
+                    this.tableType != null && this.tableType == PTableType.VIEW;
         }
     }
 
@@ -72,27 +161,6 @@ public class PhckUtil {
 
         public boolean isQualifierCounterRow() {
             return this.tableName != null && this.qualifierCounter != null;
-        }
-
-        public boolean isSystemTable() {
-            return this.tableSchema != null && this.tableSchema.equals(SYSTEM_SCHEMA_NAME) &&
-                    this.tableName != null &&
-                    this.tableType != null && this.tableType.equals(PTableType.SYSTEM);
-        }
-
-        public boolean isIndexTable() {
-            return this.tableName != null &&
-                    this. tableType != null && this.tableType.equals(PTableType.INDEX);
-        }
-
-        public boolean isPhysicalTable() {
-            return this.tableName != null &&
-                    this.tableType != null && this.tableType.equals(PTableType.TABLE);
-        }
-
-        public boolean isViewTable() {
-            return this.tableName != null &&
-                    this.tableType != null && this.tableType.equals(PTableType.VIEW);
         }
 
         public boolean isLinkRow() {
